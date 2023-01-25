b9266f6441b02083914295f748ecf4fccb543a6e
jknadan <jknadan@gmail.com>
2023-01-25 12:47:42 +0900

    [2023-01-25] ch5_DFS+BFS_p149

--- ch5_DFS+BFS_p149.py
+++ ch5_DFS+BFS_p149.py
@@ -1,33 +1,71 @@
 # N, M을 공백으로 구분하여 입력받기
 N, M = map(int, input().split())
 
-# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
-tray = [[0] * M for _ in range(N)]
+# map 정보 입력 및 저장
+tray = []
 
-# 그래프를 저장하기 위한 2차원 배열
-count = [[]]
+for i in range(N):
+    tray.append(list(map(int, input())))
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-
-def DFS(graph,x,v,visited) :
-    visited[v] = True
-    global count
-    count[x].append(v)
-    for k in graph[v]:
-        if not visited[k]:
-            DFS(graph, x, k, visited)
-
-def check(x,y):
-    global count
-    for h in range(4):
-        nx = x + dx[h]
-        ny = y + dy[h]
-        if tray[nx,ny] == 0 and count
+count = 0
 
 
-# 전체 노드 탐색
+def DFS(i, j):
+    global tray
+
+    # 현재 기준 우측이 0이고 이동 가능하면 현재를 = 1 처리하고 DFS 탐색
+    if tray[i][j + 1] == 0:
+        tray[i][j] = 1
+        DFS(i, j + 1)
+
+    # 현재 기준 아래측이 0이고 이동 가능하면 현재를 = 1 처리하고 DFS 탐색
+    if tray[i + 1][j] == 0:
+        tray[i][j] = 1
+        DFS(i + 1, j)
+
+    # Base case 혹시 몰라 예외처리 빡세게 : 그냥 return 0 해도 되나?
+    elif (tray[i + 1][j] and tray[i - 1][j] and tray[i][j + 1] and tray[i][j - 1]) != 0:
+        return 0
+
+
+def searching(i, j):
+    global tray, N, M
+    if i <= -1 or i >= N or j <= -1 or j >= M:
+        return False
+    elif tray[i][j] == 0:
+        DFS(i, j)
+        # global 키워드 쓰고 그 다음에 값이든 뭐든 할당
+        global count
+        count += 1
+
+    else:
+        return 0
+
+
 for i in range(N):
     for j in range(M):
+        searching(i, j)
 
+# 값 출력
+print(count)
 
+'''
+15 14
+00000111100000
+11111101111110
+11011101101110
+11011101100000
+11011111111111
+11011111111100
+11000000011111
+01111111111111
+00000000011111
+01111111111000
+00011111111000
+00000001111000
+11111111110011
+11100011111111
+11100011111111
+
+를 하게 되면 index Error가 뜬다. 왜 그러는 걸까?
+'''
