b72a7bb701cf6d8c35f4f61d8a5096a5a7ef7433
jknadan <jknadan@gmail.com>
2023-02-26 23:20:17 +0900

    2023-02-22 CommonProb_3

---
+++ commonProb_3.py
@@ -0,0 +1,11 @@
+N = int(input())
+rope = []
+value = []
+for i in range(N):
+    rope.append(int(input()))
+
+rope.sort(reverse=True)
+for j in range(N):
+    value.append(rope[j] * (j + 1))
+
+print(max(value))
